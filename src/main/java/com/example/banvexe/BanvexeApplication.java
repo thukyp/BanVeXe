38d2e2822b39d059da7deed455983523488af7e9
Phan Thu Ky <phanthuky12@gmail.com>
2026-03-19 10:57:59 +0700

    Thêm thành công đăng nhập với google

--- src/main/java/com/example/banvexe/BanvexeApplication.java
+++ src/main/java/com/example/banvexe/BanvexeApplication.java
@@ -3,11 +3,23 @@ package com.example.banvexe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class BanvexeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BanvexeApplication.class, args);
+		io.github.cdimascio.dotenv.Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv.configure()
+            .directory("./") 
+            .ignoreIfMissing()
+            .load();
+
+	System.out.println(">>> CHECK GOOGLE ID: " + System.getProperty("GOOGLE_CLIENT_ID"));
+
+    // Map vào System Properties
+    dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+    SpringApplication.run(BanvexeApplication.class, args);
 	}
 
 }
--- src/main/java/com/example/banvexe/config/SecurityConfig.java
+++ src/main/java/com/example/banvexe/config/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.example.banvexe.config;
 
+import com.example.banvexe.services.UserService;
+import com.example.banvexe.utils.JwtUtils;
+import jakarta.servlet.http.Cookie;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,17 +19,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // THÊM DÒNG NÀY ĐỂ TẮT CSRF
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/admin/**", "/api/**").permitAll() // Cho phép truy cập admin và api
-                        .anyRequest().permitAll());
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/login/**", "/error", "/api/**").permitAll()
+                .requestMatchers("/admin/**").permitAll() // Tạm thời permitAll để bạn làm dashboard
+                .anyRequest().authenticated()
+            )
+            .oauth2Login(oauth2 -> oauth2
+                // Khi login thành công, Spring sẽ tự gọi về đây
+                .defaultSuccessUrl("/", true) 
+            );
+            
         return http.build();
     }
 }
